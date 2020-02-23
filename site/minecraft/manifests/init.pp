15bf8d405539e87e39c25dd1fde1cf287da920c1
TM-Rin <59885407+TM-Rin@users.noreply.github.com>
2020-02-23 10:46:17 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'opt/minecraft_server.jar':
+  file {'/opt/minecraft_server.jar':
     ensure => file,
     sourse => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
   }
